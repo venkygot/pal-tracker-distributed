93d6f5b97075dd1c7c9edaf432ba6fc1f43f0668
venkygot <venky.im@gmail.com>
2020-03-15 15:57:23 +0530

    SSO Security

--- platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/App.java
+++ platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/App.java
@@ -1,13 +1,11 @@
-package io.pivotal.pal.tracker.eurekaserver;
+package io.pivotal.pal.tracker.oauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 
-
-@EnableEurekaServer
 @SpringBootApplication
-public class App {
+public class App extends AuthorizationServerConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
